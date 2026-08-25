e75401e6903e49c8b019cfe8af754f8315d7d0c3
AllaP47 <allapaschinska1@gmail.com>
2026-08-25 16:50:53 +0300

    add message

--- src/db/connectMongoDB.js
+++ src/db/connectMongoDB.js
@@ -1,13 +1,10 @@
 import mongoose from 'mongoose';
 
-// Обов'язково додаємо "export const" перед назвою функції
 export const connectMongoDB = async () => {
   try {
+  
     const mongoUrl = process.env.MONGO_URL;
-
-    if (!mongoUrl) {
-      throw new Error('MONGO_URL is not defined in environment variables');
-    }
+;
 
     await mongoose.connect(mongoUrl);
     console.log('✅ MongoDB connection established successfully');
