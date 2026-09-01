c1476306e79acaf5c7e34fa1f35b15600270e734
AllaP47 <allapaschinska1@gmail.com>
2026-09-01 17:17:54 +0300

    add message

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -1,41 +1,44 @@
 import { Router } from 'express';
 import { celebrate } from 'celebrate';
 import * as notesController from '../controllers/notesController.js';
-
 import * as schemas from '../validations/notesValidation.js';
+
+import { authenticate } from '../middleware/authenticate.js';
 
 const router = Router();
 
+
 router.get(
   '/notes',
+  authenticate,
   celebrate(schemas.getAllNotesSchema),
   notesController.getAllNotes,
 );
 
-
 router.get(
   '/notes/:noteId',
+  authenticate,
   celebrate(schemas.noteIdSchema),
   notesController.getNoteById,
 );
 
-
 router.post(
   '/notes',
+  authenticate,
   celebrate(schemas.createNoteSchema),
   notesController.createNote,
 );
 
-
 router.patch(
   '/notes/:noteId',
+  authenticate,
   celebrate(schemas.updateNoteSchema),
   notesController.updateNote,
 );
 
-
 router.delete(
   '/notes/:noteId',
+  authenticate,
   celebrate(schemas.noteIdSchema),
   notesController.deleteNote,
 );
